d4031df7d72db06887c41a31b58c00cf735da03f
cescjunior24 <91435675+cescjunior24@users.noreply.github.com>
2023-02-06 23:32:14 +0100

    Javascript done it

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    
+    var menu= document.getElementById("menu");
+
+    if(window.innerWidth<1200)
+    {
+        document.getElementById('check').checked=false;
+    }
+
+}
+
+
